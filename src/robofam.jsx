2263066b38e20ff3ff94de9dca8f68d1b5710cf0
Andrej0110 <devandrej01@gmail.com>
2025-05-13 09:30:08 +0200

    Initial commit: Add robofriends project

---
+++ src/robofam.jsx
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Damian',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'David',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Papa',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Mama',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Oma Sweta',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Opa Willi',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Oma Erna',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Opa Jura',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
